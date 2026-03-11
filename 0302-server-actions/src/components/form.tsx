8e8f5ca7c74e075ed71f41615ace18678930e8f0
raigomes <raigomes2@hotmail.com>
2026-03-11 17:54:39 -0300

    Refactoring form (remove onchange state)

--- 0302-server-actions/src/components/form.tsx
+++ 0302-server-actions/src/components/form.tsx
@@ -4,47 +4,29 @@ import login from "@/actions/login";
 import React from "react";
 
 export default function FormLogin() {
-  const [username, setUsername] = React.useState("");
-  const [password, setPassword] = React.useState("");
   const [error, setError] = React.useState("");
 
   const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const { username, password } = Object.fromEntries(formData);
 
     try {
-      const { isLogged } = await login(username, password);
-      if (!isLogged) {
-        throw new Error("Usuário não encontrado");
-      }
+      const { isLogged } = await login(username as string, password as string);
+      if (!isLogged) throw new Error("Usuário não encontrado");
       window.location.href = "/";
     } catch (err) {
-      if (err instanceof Error) {
-        setError(err.message);
-      } else {
-        setError("Ocorreu um erro inesperado");
-      }
+      setError(err instanceof Error ? err.message : "Erro inesperado");
     }
   };
 
   return (
     <form onSubmit={handleLogin}>
       <label htmlFor="username">Usuário:</label>
-      <input
-        type="text"
-        name="username"
-        id="username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
+      <input type="text" name="username" id="username" />
 
       <label htmlFor="password">Senha:</label>
-      <input
-        type="password"
-        name="password"
-        id="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
+      <input type="password" name="password" id="password" />
 
       <button>Login</button>
 
